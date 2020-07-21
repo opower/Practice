ce16dee4cf581261b7d22eaf825da4bb80005402
Olivia <oliviapower.02@gmail.com>
2020-07-21 15:10:11 -0700

    triangle sum

---
+++ triangle.js
@@ -0,0 +1,15 @@
+const missingAngle = (num1, num2) => {
+
+  const sum = 180;
+  const angle = num1 + num2
+  if(sum - angle  < 90){
+    return 'acute'
+  } else if (sum - angle === 90){
+    return 'right'
+  } else {
+    return 'obtuse'
+  }
+}
+console.log(missingAngle(27, 59))
+console.log(missingAngle(135, 11))
+console.log(missingAngle(45, 45))
